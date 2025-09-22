49cecd6413ce71c238b578b69026ad9522b44d5e
mohiyuddin <mohiyuddin.shaikh@tradofina.com>
2025-09-22 15:35:51 +0530

    solution: counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-// App.jsx
-
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="">Solution goes here</div>;
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+
+  return (
+    <div>
+      <button data-testid="decrement-button" onClick={decrement}>
+        -
+      </button>
+      <button data-testid="increment-button" onClick={increment}>
+        +
+      </button>
+      <p>clicked: {count}</p>
+    </div>
+  );
 }
 
-export default App;
+// run your code by clicking the run button on the right
